f76a4ef28bd611a8185ba7476fe79ddf5e287715
GabrielGajdos <gabriel.gajdos@student.ukf.sk>
2025-03-06 22:27:05 +0100

    QnA a Otazky

---
+++ otazky.php
@@ -0,0 +1,11 @@
+$otazky = array(
+    "Aké sú vaše skúsenosti s PHP?",
+    "Čo si myslíte o programovaní?",
+    "Obľúbená farba?"
+);
+
+$odpovede = array(
+    "Mám začiatočnícke znalosti PHP.",
+    "Je to užitočný skill.",
+    "Modrá,."
+);
--- qna.php
+++ qna.php
@@ -41,6 +41,13 @@
       </div>
     </section>
       <section class="container">
+      <?php include "otazky.php"; ?>    
+      <?php for ($i = 0; $i < count($otazky); $i++) { ?>        
+      <div class="question"><?php echo $otazky[$i]; ?></div>            
+      <div class="answer"><?php echo $odpovede[$i]; ?></div>       
+      </div>    
+      <?php } ?>
+      </section>
       <div class="accordion">
         <div class="question">Otázka 1</div>
         <div class="answer">Odpoveď 1</div>
